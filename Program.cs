1c80bec3e10bf4d6624adb8778c3ff1ae149c558
royjianxue <royjianxue@yahoo.com>
2022-04-11 18:07:49 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace ListsHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string answer;
+            bool keepGoing = false;
+            string firstName;
+            List<string> myClass = new List<string> ();
+
+            do
+            {
+                System.Console.Write("Please enter students first name: ");
+                firstName = Console.ReadLine();
+                myClass.Add(firstName);
+                Console.Write("Do you have more students to be added? Yes --> continue.. ANY --> stop");
+                answer = Console.ReadLine();
+                if (answer.ToUpper() == "YES")
+                {
+                    keepGoing = true;
+                }
+                else
+                {
+                    keepGoing = false;
+                }
+            } while (keepGoing);
+            Console.WriteLine($"There are {myClass.Count} students in this class.");
+        }
+    }
+}
